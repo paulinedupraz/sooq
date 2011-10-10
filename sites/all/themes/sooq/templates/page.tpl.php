cba6fbc0e923a35aa66426c9bf89a1b71529b267
Pauline Dupraz <duprazpauline@gmail.com>
2011-10-10 18:26:28 +0200

    remove breadcrumb, headings, secondary menu

--- sites/all/themes/sooq/templates/page.tpl.php
+++ sites/all/themes/sooq/templates/page.tpl.php
@@ -104,21 +104,18 @@
 
     <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section">
-          <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-          <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+          <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'grid_12')), 'heading' => '')); ?>
+          <?php if ($page['highlighted']): ?><div id="highlighted" class="grid_12 clearfix"><?php print render($page['highlighted']); ?></div><?php endif; ?>
         </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
 
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
+   
 
     <?php print $messages; ?>
 
     <div id="main-wrapper"><div id="main" class="clearfix">
 
         <div id="content" class="column grid_9"><div class="section">
-            <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
             <a id="main-content"></a>
             <?php print render($title_prefix); ?>
             <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
